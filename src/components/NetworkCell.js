6d7c378a50c3e6975cf26471cc2e621266605d92
braddiver <braddiver@gmail.com>
2024-04-17 16:53:51 -0400

    focus

--- src/components/NetworkCell.js
+++ src/components/NetworkCell.js
@@ -3,10 +3,25 @@ import Blits from '@lightningjs/blits'
 export default Blits.Component('NetworkCell', {
   props: ['networkName', 'number'],
 
+  state() {
+    return {
+      color: '#222'
+    }
+  },
+
   template: `
-    <Element w="300" h="100" color="#000">
+    <Element ref="el" w="300" h="100" :color="$color">
       <Text x="32" y="32" :content="$networkName"/>
       <Text align="right" x="240" y="32" :content="$number"/>
     </Element>
   `,
+
+  hooks: {
+    focus() {
+      this.color = '#000';
+    },
+    unfocus() {
+      this.color = '#222';
+    }
+  }
 })
--- src/components/NetworkColumn.js
+++ src/components/NetworkColumn.js
@@ -10,6 +10,7 @@ export default Blits.Component('NetworkColumn', {
 
   state() {
     return {
+      foc: 0,
       network0: { name: '', callSign: '' },
       network1: { name: '', callSign: '' },
       network2: { name: '', callSign: '' },
@@ -20,11 +21,11 @@ export default Blits.Component('NetworkColumn', {
 
   template: `
     <Element>
-      <NetworkCell y="0"   :networkName="$network0.name" :number="$network0.callSign" />
-      <NetworkCell y="100" :networkName="$network1.name" :number="$network1.callSign" />
-      <NetworkCell y="200" :networkName="$network2.name" :number="$network2.callSign" />
-      <NetworkCell y="300" :networkName="$network3.name" :number="$network3.callSign" />
-      <NetworkCell y="400" :networkName="$network4.name" :number="$network4.callSign" />
+      <NetworkCell ref="el0" y="0"   :networkName="$network0.name" :number="$network0.callSign" />
+      <NetworkCell ref="el1" y="100" :networkName="$network1.name" :number="$network1.callSign" />
+      <NetworkCell ref="el2" y="200" :networkName="$network2.name" :number="$network2.callSign" />
+      <NetworkCell ref="el3" y="300" :networkName="$network3.name" :number="$network3.callSign" />
+      <NetworkCell ref="el4" y="400" :networkName="$network4.name" :number="$network4.callSign" />
     </Element>
   `,
 
@@ -37,10 +38,31 @@ export default Blits.Component('NetworkColumn', {
         cell.callSign = callSign;
       }
     },
+
+    updateFocus() {
+      this.select(`el${this.foc}`).focus();
+    }
+  },
+
+  input: {
+    up() {
+      if (this.foc > 0) {
+        this.foc --;
+        this.updateFocus();
+      }
+    },
+
+    down() {
+      if (this.foc < 5) {
+        this.foc ++;
+        this.updateFocus();
+      }
+    }
   },
 
   hooks: {
     ready() {
+      this.updateFocus();
       this.update();
     }
   }
