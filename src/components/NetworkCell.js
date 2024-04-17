6b524a5d6032b1b2dd240587b46bcc0689fe1619
braddiver <braddiver@gmail.com>
2024-04-17 12:18:46 -0400

    cleanup

--- src/components/NetworkCell.js
+++ src/components/NetworkCell.js
@@ -1,12 +1,12 @@
 import Blits from '@lightningjs/blits'
 
 export default Blits.Component('NetworkCell', {
+  props: ['networkName', 'number'],
+
   template: `
     <Element w="300" h="100" color="#000">
       <Text x="32" y="32" :content="$networkName"/>
       <Text align="right" x="240" y="32" :content="$number"/>
     </Element>
   `,
-
-  props: ['networkName', 'number'],
 })
