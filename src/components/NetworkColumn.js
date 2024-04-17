d48752cda7edc2f0e85d36701d46c98cb340d146
braddiver <braddiver@gmail.com>
2024-04-17 11:49:40 -0400

    pass network data to column

--- src/components/NetworkColumn.js
+++ src/components/NetworkColumn.js
@@ -6,13 +6,47 @@ export default Blits.Component('NetworkColumn', {
     NetworkCell
   },
 
+  props: ['networks'],
+
+  state() {
+    return {
+      network0: '',
+      network1: '',
+      network2: '',
+      network3: '',
+      network4: '',
+      callSign0: '',
+      callSign1: '',
+      callSign2: '',
+      callSign3: '',
+      callSign4: '',
+    }
+  },
+
   template: `
     <Element>
-      <NetworkCell networkName="ESPN" number="6" />
-      <NetworkCell y="100" networkName="CNN" number="7" />
-      <NetworkCell y="200" networkName="PIG" number="8" />
-      <NetworkCell y="300" networkName="NBC" number="9" />
-      <NetworkCell y="400" networkName="ABC" number="11" />
+      <NetworkCell y="0"   :networkName="$network0" :number="$callSign0" />
+      <NetworkCell y="100" :networkName="$network1" :number="$callSign1" />
+      <NetworkCell y="200" :networkName="$network2" :number="$callSign2" />
+      <NetworkCell y="300" :networkName="$network3" :number="$callSign3" />
+      <NetworkCell y="400" :networkName="$network4" :number="$callSign4" />
     </Element>
-  `
-})
+  `,
+
+  methods: {
+    update(index = 0) {
+      for (let i = 0; i < 5; i++) {
+        const [networkName, callSign] = this.networks[index + i];
+        this[`network${i}`] = networkName;
+        this[`callSign${i}`] = callSign;
+      }
+    },
+  },
+
+  hooks: {
+    ready() {
+      console.log({networks: this.networks})
+      this.update();
+    }
+  }
+});
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,9 +8,27 @@ export default Blits.Component('Home', {
     Loader,
     NetworkColumn
   },
+
   template: `
     <Element w="1920" h="1080" color="#333">
-      <NetworkColumn x="200" y="200" />
+      <NetworkColumn :networks="$networks" x="200" y="200" />
     </Element>
   `,
-})
+
+  hooks: {
+    init() {
+      this.networks = [
+        [ 'A', 7 ],
+        [ 'B', 8 ],
+        [ 'C', 9 ],
+        [ 'D', 10 ],
+        [ 'E', 11 ],
+        [ 'F', 12 ],
+        [ 'G', 13 ],
+        [ 'H', 14 ],
+        [ 'I', 15 ],
+        [ 'J', 16 ]
+      ];
+    }
+  },
+});
